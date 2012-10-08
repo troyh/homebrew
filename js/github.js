c9acea2d8d6f62a80cccef213039a868fafd351a
Troy Hakala <troy.hakala@gmail.com>
2012-10-08 16:23:02 -0700

    Starting on getting github saving

---
+++ js/github.js
@@ -0,0 +1,94 @@
+var access_token="5b718b4156cce4d7929c4309980d273a1f7f69bc";
+
+function github_commit(repo,files,message) {
+
+	console.log(files);
+	var files_tree=[];
+	for (var i=0; i< files.length; ++i) {
+		files_tree.push({
+			path: files[i].filepath,
+			mode: "100644",
+			type: "blob",
+			content: files[i].content
+		});
+	}
+	
+	$.ajaxSetup({
+		beforeSend: function(jqXHR,settings) {
+			console.log("AJAX:" + settings.url);
+		},
+		headers: {
+			"Authorization": "token " + access_token
+		}
+	})
+
+	$.getJSON("https://api.github.com/repos/"+repo.user+"/"+repo.repo+"/git/refs/heads/"+repo.branch,null,function(ref) {
+		console.log("refs response:");
+		console.log(ref);
+		$.getJSON("https://api.github.com/repos/"+repo.user+"/"+repo.repo+"/git/trees/"+ref.object.sha,null,function(tree) {
+			console.log("trees response:");
+			console.log(tree);
+			var postdata=				{
+					base_tree: tree.sha,
+					tree: files_tree
+				};
+				console.log("postdata:");
+				console.log(postdata);
+			$.post("https://api.github.com/repos/"+repo.user+"/"+repo.repo+"/git/trees",
+				postdata,
+				function(new_tree) {
+					console.log("trees response:");
+					console.log(new_tree);
+					$.post("https://api.github.com/repos/"+repo+"/"+repo.repo+"/git/commits",
+						{
+							message: message,
+							tree: new_tree.sha,
+							parents: [ ref.object.sha ]
+						},
+						function(commit) {
+							console.log("Commit response:");
+							console.log(commit);
+							$.post("https://api.github.com/repos/"+repo+"/"+repo.repo+"/git/refs/"+ref.object.sha,
+								{
+										sha: commit.sha
+								},
+								function(ref) {
+										console.log(ref);
+								}
+							)
+						},
+						"json"
+					);
+				},
+				"json"
+			);
+		});
+	});
+	
+	// for (var file in files) {
+	// 	var pathparts=file.filepath.split("/");
+	// 	var filename=pathparts.pop();
+	// 	var dirname=pathparts;
+	// 	
+	// 	// Find the current place in the tree for this filepath (if it exists)
+	// 	var found=false;
+	// 	for (obj in tree.tree) {
+	// 		if (obj.type=="blob" && obj.path==file.filepath) {
+	// 			// Change the sha
+	// 			obj.content=file.content;
+	// 			delete obj.sha;
+	// 			found=true;
+	// 			break;
+	// 		}
+	// 	}
+	// 	if (!found) {
+	// 		// Create an object in the tree
+	// 		tree.tree.push({
+	// 			type: "blob",
+	// 			path: file.filepath,
+	// 			mode: "100644",
+	// 			content=file.content
+	// 		});
+	// 	}
+	// }
+}
