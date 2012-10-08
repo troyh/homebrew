6020c5455f78379a91f916052f10bdc12a314363
Troy Hakala <troy.hakala@gmail.com>
2012-10-08 16:46:43 -0700

    Github saving works!!

--- js/github.js
+++ js/github.js
@@ -1,8 +1,22 @@
 var access_token="5b718b4156cce4d7929c4309980d273a1f7f69bc";
 
+jQuery.extend({
+   postJSON: function(url, data, callback) {
+     return jQuery.ajax({
+       type: "POST",
+       url: url,
+       data: JSON.stringify(data),
+       success: callback,
+       dataType: "json",
+       contentType: "application/json",
+       processData: false
+     });
+   }
+ });
+ 
 function github_commit(repo,files,message) {
 
-	console.log(files);
+	// console.log(files);
 	var files_tree=[];
 	for (var i=0; i< files.length; ++i) {
 		files_tree.push({
@@ -15,7 +29,7 @@ function github_commit(repo,files,message) {
 	
 	$.ajaxSetup({
 		beforeSend: function(jqXHR,settings) {
-			console.log("AJAX:" + settings.url);
+			// console.log("AJAX:" + settings.url);
 		},
 		headers: {
 			"Authorization": "token " + access_token
@@ -23,37 +37,37 @@ function github_commit(repo,files,message) {
 	})
 
 	$.getJSON("https://api.github.com/repos/"+repo.user+"/"+repo.repo+"/git/refs/heads/"+repo.branch,null,function(ref) {
-		console.log("refs response:");
-		console.log(ref);
+		// console.log("refs response:");
+		// console.log(ref);
 		$.getJSON("https://api.github.com/repos/"+repo.user+"/"+repo.repo+"/git/trees/"+ref.object.sha,null,function(tree) {
-			console.log("trees response:");
-			console.log(tree);
+			// console.log("trees response:");
+			// console.log(tree);
 			var postdata=				{
 					base_tree: tree.sha,
 					tree: files_tree
 				};
-				console.log("postdata:");
-				console.log(postdata);
-			$.post("https://api.github.com/repos/"+repo.user+"/"+repo.repo+"/git/trees",
+				// console.log("postdata:");
+				// console.log(postdata);
+			$.postJSON("https://api.github.com/repos/"+repo.user+"/"+repo.repo+"/git/trees",
 				postdata,
 				function(new_tree) {
-					console.log("trees response:");
-					console.log(new_tree);
-					$.post("https://api.github.com/repos/"+repo+"/"+repo.repo+"/git/commits",
+					// console.log("trees response:");
+					// console.log(new_tree);
+					$.postJSON("https://api.github.com/repos/"+repo.user+"/"+repo.repo+"/git/commits",
 						{
 							message: message,
 							tree: new_tree.sha,
 							parents: [ ref.object.sha ]
 						},
 						function(commit) {
-							console.log("Commit response:");
-							console.log(commit);
-							$.post("https://api.github.com/repos/"+repo+"/"+repo.repo+"/git/refs/"+ref.object.sha,
+							// console.log("Commit response:");
+							// console.log(commit);
+							$.postJSON("https://api.github.com/repos/"+repo.user+"/"+repo.repo+"/git/refs/heads/"+repo.branch,
 								{
 										sha: commit.sha
 								},
 								function(ref) {
-										console.log(ref);
+										// console.log(ref);
 								}
 							)
 						},
